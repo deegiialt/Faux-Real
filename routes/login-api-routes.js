392bb226c84521cf8f6025cbc60d834ff0f58e38
Samuel Mast <samuel.a.mast@gmail.com>
2018-03-08 14:34:56 -0700

    other authentication changes

--- routes/login-api-routes.js
+++ routes/login-api-routes.js
@@ -22,7 +22,7 @@
       })
     })
 
-    app.post('/login', passport.authenticate('local-login', { successRedirect: '/main',failureRedirect: '/' }));
+    app.post('/login', passport.authenticate('local-login', { successRedirect: '/main', failureRedirect: '/' }));
 
     app.get('/logout', (req, res)=>{
       req.logout();
