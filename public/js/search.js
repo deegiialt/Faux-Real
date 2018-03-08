ebd1f0b8cb299205b64d5e93097f25aab922475d
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-08 11:57:31 -0700

    all source search options function

--- public/js/search.js
+++ public/js/search.js
@@ -25,7 +25,7 @@ $("#search-submit").on("click", function(){
       }else if($('#usa-today').is(':checked')){
         searchUSA();
       }else if($('#google-news').is(':checked')){
-        searchGogle();
+        searchGoogle();
       }else if($('#buzzfeed').is(':checked')){
         searchBuzzfeed();
       }
@@ -47,22 +47,12 @@ $("#entertainment").on("click", function(){
       searchEntertainment();
 })
 
+$("#technology").on("click", function(){
+      $("#div-section").empty();
+      searchTech();
+})
 
-//trending tab search
 
-function searchTopHeadlines(){
-      var url = 'https://newsapi.org/v2/top-headlines?' +
-              'country=us&' +
-              'apiKey=b8438b744ce24b42a60f3a56367a00c4';
-      $.ajax({
-        url: url,
-        method: "GET",
-
-      }).then(function(response){
-        console.log(response);
-        return renderArticles(response.articles);
-      });
-};
 
 
 //the next 8 functions are for the search option menu
@@ -133,13 +123,145 @@ function searchWallStreet(){
       });
 }
 
+function searchHuffPost(){
+
+  var keyword = $(".form-control").val().trim();
+
+  var url = 'https://newsapi.org/v2/everything?' +
+              'q=' + keyword + '&' +
+              'sources=the-huffington-post&' +
+              'language=en&' +
+              'sortBy=publishedAt&' +
+              'sortBy=relevancy&' +
+              'sortBy=popularity&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4';
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+}
+
+function searchVice(){
+
+  var keyword = $(".form-control").val().trim();
+
+  var url = 'https://newsapi.org/v2/everything?' +
+              'q=' + keyword + '&' +
+              'sources=vice-news&' +
+              'language=en&' +
+              'sortBy=publishedAt&' +
+              'sortBy=relevancy&' +
+              'sortBy=popularity&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4';
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+}
+
+function searchUSA(){
+
+  var keyword = $(".form-control").val().trim();
+
+  var url = 'https://newsapi.org/v2/everything?' +
+              'q=' + keyword + '&' +
+              'sources=usa-today&' +
+              'language=en&' +
+              'sortBy=publishedAt&' +
+              'sortBy=relevancy&' +
+              'sortBy=popularity&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4';
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+}
+
+function searchGoogle(){
+
+  var keyword = $(".form-control").val().trim();
+
+  var url = 'https://newsapi.org/v2/everything?' +
+              'q=' + keyword + '&' +
+              'sources=google-news&' +
+              'language=en&' +
+              'sortBy=publishedAt&' +
+              'sortBy=relevancy&' +
+              'sortBy=popularity&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4';
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+}
+
+function searchBuzzfeed(){
+
+  var keyword = $(".form-control").val().trim();
+
+  var url = 'https://newsapi.org/v2/everything?' +
+              'q=' + keyword + '&' +
+              'sources=buzzfeed&' +
+              'language=en&' +
+              'sortBy=publishedAt&' +
+              'sortBy=relevancy&' +
+              'sortBy=popularity&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4';
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+}
+//================================================================
+//================================================================
+//================================================================
+
+//the remainder of the functions are for the tab options on the page
 //business tab search
+
+//trending tab search
+
+function searchTopHeadlines(){
+      var url = 'https://newsapi.org/v2/top-headlines?' +
+              'country=us&' +
+              'sortBy=publishedAt&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4';
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+};
 
 function searchBusiness(){
 
   var url = 'https://newsapi.org/v2/top-headlines?' +
               'country=us&' +
               'category=business&' +
+              'sortBy=publishedAt&' +
               'apiKey=b8438b744ce24b42a60f3a56367a00c4'
       $.ajax({
         url: url,
@@ -158,6 +280,7 @@ function searchSports(){
       var url = 'https://newsapi.org/v2/top-headlines?' +
               'country=us&' +
               'category=sports&' +
+              'sortBy=publishedAt&' +
               'apiKey=b8438b744ce24b42a60f3a56367a00c4'
       $.ajax({
         url: url,
@@ -175,6 +298,7 @@ function searchEntertainment(){
     var url = 'https://newsapi.org/v2/top-headlines?' +
               'country=us&' +
               'category=entertainment&' +
+              'sortBy=publishedAt&' +
               'apiKey=b8438b744ce24b42a60f3a56367a00c4'
       $.ajax({
         url: url,
@@ -184,11 +308,27 @@ function searchEntertainment(){
         console.log(response);
         return renderArticles(response.articles);
       });
-
-
-
 }
 
+function searchTech(){
+    var url = 'https://newsapi.org/v2/top-headlines?' +
+              'country=us&' +
+              'category=technology&' +
+              'sortBy=publishedAt&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4'
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+}
+
+//========================================================
+//========================================================
+//========================================================
 //this function will render all other search functions
 
 function renderArticles(searchResponse){
@@ -200,7 +340,7 @@ function renderArticles(searchResponse){
       var newPanelHeading = $("<div>");
       newPanelHeading.attr("class", "panel-heading")
       newPanelHeading.attr("id", "articleHead-" + i)
-      newPanelHeading.append("<h3>" + searchResponse[i].source.name + "</h>");
+      newPanelHeading.append("<h3>" + searchResponse[i].source.name + "</h3>");
       newPanelHeading.append("<h4>" + searchResponse[i].author + "</h4>");
       var newPanelBody = $("<div>");
       newPanelBody.attr("class", "panel-body")
