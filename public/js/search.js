7219ad5824978aab4410530fb07a8a0960b26e42
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-07 21:34:21 -0700

    all flinks working minus international

--- public/js/search.js
+++ public/js/search.js
@@ -9,18 +9,33 @@ $(document).ready(function(){
 //var newsAPI = keys.apiNewsKey;
 
  $("#trending").on("click", function(){
-
      searchTopHeadlines();
-
 })
+
+$("#search-submit").on("click", function(){
+      searchKeyword();
+      renderArticles().clear();
+});
+
+$("#business").on("click", function(){
+      searchBusiness();
+});
+
+$("#sports").on("click", function(){
+      searchSports();
+})
+
+$("#entertainment").on("click", function(){
+      searchEntertainment();
+})
+
+
+//trending tab search
 
 function searchTopHeadlines(){
       var url = 'https://newsapi.org/v2/top-headlines?' +
               'country=us&' +
               'apiKey=b8438b744ce24b42a60f3a56367a00c4';
-
-      var req = new Request(url);
-
       $.ajax({
         url: url,
         method: "GET",
@@ -31,9 +46,91 @@ function searchTopHeadlines(){
       });
 };
 
+
+
+//seach bar function
+
+function searchKeyword(){
+
+  var keyword = $(".form-control").val().trim();
+
+  var url = 'https://newsapi.org/v2/everything?' +
+              'q=' + keyword + '&' +
+              'language=en&' +
+              'sortBy=relevancy&' +
+              'sortBy=popularity&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4';
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+}
+
+//business tab search
+
+function searchBusiness(){
+
+  var url = 'https://newsapi.org/v2/top-headlines?' +
+              'country=us&' +
+              'category=business&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4'
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+
+}
+
+//sports tab search
+
+function searchSports(){
+      var url = 'https://newsapi.org/v2/top-headlines?' +
+              'country=us&' +
+              'category=sports&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4'
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+}
+
+//entertainment tab search
+
+function searchEntertainment(){
+    var url = 'https://newsapi.org/v2/top-headlines?' +
+              'country=us&' +
+              'category=entertainment&' +
+              'apiKey=b8438b744ce24b42a60f3a56367a00c4'
+      $.ajax({
+        url: url,
+        method: "GET",
+
+      }).then(function(response){
+        console.log(response);
+        return renderArticles(response.articles);
+      });
+
+
+
+}
+
+//this function will render all other search functions
+
 function renderArticles(searchResponse){
 
-    for(var i = 0; i < 10; i++){
+    for(var i = 0; i < searchResponse.length; i++){
 
       var newPanel = $("<div>"); 
       var newPanelHeading = $("<div>");
@@ -54,17 +151,6 @@ function renderArticles(searchResponse){
 
     };
 };
-
-
-
-function searchInternational(){
-    
-
-
-}
-
-
-
 
 
 //searchTopHeadlines();
