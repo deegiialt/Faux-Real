a769128d4f4a05f579e472f5c943bb1048cf8426
Samuel Mast <samuel.a.mast@gmail.com>
2018-03-11 01:49:13 -0700

    added google authentication, pulled in username using ejs

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    // 'facebookAuth' : {
+    //     'clientID'      : 'your-secret-clientID-here', // your App ID
+    //     'clientSecret'  : 'your-client-secret-here', // your App Secret
+    //     'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    // },
+
+    'googleAuth' : {
+        'clientID'      : '853191641691-h2urnb5igp2h0p546vt133rr6lokrn3v.apps.googleusercontent.com',
+        'clientSecret'  : 'NJirR1EfU4pyxxb9kX4p9dWz',
+        'callbackURL'   : 'http://localhost:8080/main'
+    }
+
+};
--- routes/login-api-routes.js
+++ routes/login-api-routes.js
@@ -20,8 +20,23 @@
         successRedirect: '/main',
         failureRedirect: '/', 
         failureFlash : true })// allow flash messages
-
     );
+
+    // =====================================
+    // GOOGLE ROUTES =======================
+    // =====================================
+    // send to google to do the authentication
+    // profile gets us their basic information including their name
+    // email gets their emails
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+            passport.authenticate('google', {
+                    successRedirect : '/profile',
+                    failureRedirect : '/'
+            }));
+
 
   };
 
