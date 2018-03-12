b3449e5987c8084fd1d50c7b80d211298cab62cc
Zack <zmoumen13@gmail.com>
2018-03-11 18:24:49 -0600

    no changes made

--- routes/login-api-routes.js
+++ routes/login-api-routes.js
@@ -1,4 +1,5 @@
   var db = require("../models");
+  var User = require("../models").User;
   const passport = require('passport');
 
   // Routes
@@ -7,26 +8,29 @@
 
 
     app.post("/signup", function(req, res) {
-       
-        passport.authenticate('local-signup')(req, res, function() {
-          res.redirect('/main')
-        })
-      
-    })
+      passport.authenticate('local-signup')(req, res, function() {
+        res.redirect('/main')
+      });
+    });
 
     //This isnt working:
-    app.post('/login', 
-      passport.authenticate('local-login', {  
+    app.post('/login',
+      passport.authenticate('local-login', {
         successRedirect: '/main',
-        failureRedirect: '/', 
+        failureRedirect: '/',
         failureFlash : true })// allow flash messages
-
     );
 
   };
 
 
-
-
-
-
+//Keep code for testing purposes
+// app.get('/login', function(req, res) {
+//   User.findOne({
+//     where: {
+//       userName: req.body
+//     }
+//   }).then(function(user) {
+//     console.log(user);
+//   });
+// })
